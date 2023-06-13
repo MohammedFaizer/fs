2f4a159e9f269bc45f6ea3f3ceff198f1ac0047e
MohammedFaizer <zmd5zer@gmail.com>
2023-06-13 16:16:33 +0530

    initial

--- Four/src/App.jsx
+++ Four/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import cit from'./citiy.json'
 import './App.css'
-
+import green from './assets/greenpin.png'
+import red from './assets/pin.png'
+import calendar from './assets/calendar.png'
 function App() {
-  const [count, setCount] = useState(0)
-
+ const autocomplete=(cit)=>{
+  alert.log(cit);
+ }
+  const[show,setshow]=useState(false);
+  const [pickup,setPickup]=useState('');
+  const[drop,setDrop]=useState('');
+  //const sugession=cit.filter(cit()=>cit.);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+   <>
+<h1>Go Taxi</h1>
+<img src={green}></img>
+    <input type= 'text' placeholder='pickup' onChange={(e)=>setPickup(e.target.value)} onFocus={()=>setshow(true)} ></input>
+    <br></br>
+    <img src={red}></img>
+    <input type= 'text' placeholder='drop' onChange={(e)=>setDrop(e.target.value)} ></input>
+    <br></br>
+    <img src={calendar}></img>
+    <input type='datetime-local' id='cl' ></input>
 
-export default App
+  
+    <br></br>
+   
+    <select >
+   <option value="Suv">Suv</option>
+   <option value="HatchBack">Hatchback</option>
+   <option value="Sedan">Sedan</option>
+    </select>
+<br></br>
+    <button type='submit' >Book Ride</button>
+  
+   {show && (
+    <ul>
+        {
+            sugession.map(sugession=>
+                {
+                    <li key={sugession}/>
+                })
+        }
+    </ul>
+   )}
+    </> 
+      )
+    }
+    
+    export default App
--- One/src/App.jsx
+++ One/src/App.jsx
@@ -3,31 +3,118 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function Item({ message, value = 0.0 }) {
+  return (
+    <div className='item'>
+      <div className='item__left'>{message}</div>
+      <div className='item__right'>{value}</div>
+    </div>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+const[unit,setUnit]=useState(0);
+const [netAmount, setNetAmount] = useState(0)
+const [currentCharge, setCurrentCharge] = useState(0)
+const nullcond = () => {
+  if (unit <= 0 ) {
+    alert('Enter Valid Units')
+    return
+  }
+  
+    setNetAmount(calculate(unit))
+  
+}
+const calculate=(e)=>{
+  let subsidy = 250
+  let f = 0
+
+  if (e >= 0 && e <= 100) f = 0
+  else if (e > 100 && e <= 150) f = e*3.75
+  else if (e > 150 && e <= 200) f = 100 + 3.75 * e
+  else if (e > 200 && e <= 350) f =e*4
+  else if (e> 350 && e <= 400) f = 250 + 4 * e
+  else if (e > 400 && e <= 450) f = 4.25*e
+  else if (e > 450 && e <= 600) f = 300 + 4.25 * e
+  else f = 400 + 5 * e 
+
+
+
+  if (f < 250) subsidy = 0
+
+  setCurrentCharge(f)
+  return f + 50 - subsidy
+}
+
+
 
   return (
+
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+
+        <h1>. . Shan Tech . .</h1>
+        <input type='number' placeholder='Enter the Units' id='units' onChange={(e) => setUnit(Number(e.target.value))}></input>
+        <br></br>
+        <button type="submit" onClick={nullcond}>check</button>
+        <br></br>
+        <center>
+        <table border='1px'>
+
+          <tr>
+            <td>Consumed Units</td>
+            <td>{unit}</td>
+          </tr>
+          <tr>
+            <td>Total Current Charge Rs:</td>
+            <td>{currentCharge}</td>
+          </tr>
+          <tr>
+            <td>Current Charge Rs.</td>
+            <td>{currentCharge}</td>
+          </tr>
+          <tr>
+            <td>Fixed Cost Rs.(+)</td>
+            <td>{50}</td>
+          </tr>
+          <tr>
+            <td>Subsidy Fixed Cost Rs.(-)s</td>
+            <td>{0}</td>
+          </tr>
+
+          <tr>
+            <td>New Subsidy Rs.(-)</td>
+            <td>{250}</td>
+          </tr>
+          
+          <tr>
+         
+            <td>Net Amount</td>
+            <td>{netAmount}</td>
+          </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </table>
+        </center>
+      
     </>
   )
 }
